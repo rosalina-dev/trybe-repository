84584f0974d3e1d695063ac12abe2fb9add017cd
João Rosalina <jp.rosalina-dev@gmail.com>
2023-01-24 13:47:18 -0300

    resolvendo todos exercicios menos o do xadrez

--- introduçao-a-javascript/dia-1/script.js
+++ introduçao-a-javascript/dia-1/script.js
@@ -26,3 +26,58 @@ console.log(f / g);
 let i = 4
 let j = 12
 console.log(i % j);
+
+console.log('Utilize if/else para escrever um código que retorne o maior de dois números. Defina, no começo do seu código, duas constantes com os valores que serão comparados.')
+
+const num8 = 45
+const num9 = 95
+if (num8 > num9) {
+  console.log('O primeiro número é maior')
+}
+else if (num8 < num9) {
+  console.log('O segundo número é maior')
+}
+else if (num8 = num9) {
+  console.log('Os números são iguais!')
+}
+
+console.log('Utilize if/else para escrever um código que retorne o maior de três números. Defina, no começo do seu código, três constantes com os valores que serão comparados.')
+
+const num5 = 95
+const num6 = 46
+const num7 = 95
+
+if (num5 > num7 && num5 > num6) {
+  console.log('O primeiro número é maior')
+}
+else if (num6 > num7 && num6 > num5) {
+  console.log('O segundo número é maior')
+}
+else if (num7 > num6 && num7 > num5) {
+  console.log('O terceiro número é maior')
+}
+
+const ra = -4
+if (ra > 0) {
+  console.log("positive");
+} else if ( ra < 0) {
+  console.log('negative');
+} else {
+  console.log('zero');
+}
+
+const ag1 = 90
+const ag2 = 43
+const ag3 = -47
+let somaDosAng = ag1 + ag2 + ag3
+let angPositivos = ag1 > 0 && ag2 > 0 && ag3 > 0
+
+if (angPositivos) {
+  if (somaDosAng === 180) {
+    console.log(true);
+  } else {
+    console.log(false)
+  }
+} else {
+  console.log('Erro: angulo invalido');
+};
