95b10917ba47c4d16196f763170883706d115451
João Rosalina <jp.rosalina-dev@gmail.com>
2023-01-24 11:53:33 -0300

    resolvendo primeiro exercicio

---
+++ introduçao-a-javascript/dia-1/script.js
@@ -0,0 +1,28 @@
+///Nos exercícios de hoje, você vai elaborar alguns códigos de acordo com o que é pedido no enunciado. Todos trabalham a lógica condicional (if/else e switch/case), operadores aritméticos (+, -, *, /, %) e operadores lógicos (>, <, &&, ||). Para que consiga executar seus códigos, recomendamos que utilize a extensão Code Runner. Você pode ver mais sobre ela no conteúdo que fizemos sobre o uso do VS Code.
+
+//Elabore alguns códigos e imprima o resultado no console usando o console.log, um para cada operação aritmética básica. Seu código deve ter duas constantes, a e b, definidas no começo com os valores que serão operados. Escreva códigos para:
+
+//Adição (a + b)
+let a = 3;
+let b = 2;
+console.log(a + b);
+
+//Subtração (c - d)
+let c = 332452;
+let d = 223452;
+console.log(c - d);
+
+//Multiplicaçao (e * f)
+let e = 32;
+let f = 2234;
+console.log(e * f);
+
+//Divisao (g / h)
+let g = 45
+let h = 6
+console.log(f / g);
+
+//Módulo (i % j)
+let i = 4
+let j = 12
+console.log(i % j);
