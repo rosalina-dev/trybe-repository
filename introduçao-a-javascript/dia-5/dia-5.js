93010f56f43cc1db488af40c682241e1f0a8e901
João Rosalina <jp.rosalina-dev@gmail.com>
2023-01-31 07:49:26 -0300

    finalizando dia 4

---
+++ introduçao-a-javascript/dia-5/dia-5.js
@@ -0,0 +1,18 @@
+let favoriteLanguage = 'Javascript';
+favoriteLanguage = 'Python';
+console.log(favoriteLanguage); // Erro
+
+let favoriteTechnology = 'Machine learning';
+favoriteTechnology = 'Facial recognition';
+console.log(favoriteTechnology); // Facial recognition
+
+
+//
+// Com o template literals
+console.log(`Primeira linha; 
+Segunda linha; 
+Terceira linha;`
+);
+
+// Sem o template literals:
+console.log('Primeira linha;\n' + 'Segunda linha;\n' + 'Terceira linha;\n');
